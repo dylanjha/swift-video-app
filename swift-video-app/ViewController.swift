6f2ec56320f5f96d2b76e31ba0953128bf85742f
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2021-05-12 09:04:42 -0700

    Initial Commit

---
+++ swift-video-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-video-app
+//
+//  Created by Dylan Jhaveri on 5/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
